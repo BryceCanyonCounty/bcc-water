6c917a23257773498bd8bafb763a3f58a5130cab
JusCampin <37031311+JusCampin@users.noreply.github.com>
2023-03-21 02:11:55 -0700

    removed items / changed label water.sql

--- water.sql
+++ water.sql
@@ -1,9 +1,1 @@
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('canteenempty', 'Canteen (Empty)', 1, 1, 'item_standard', 1);
-
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('canteen', 'Canteen (Full)', 1, 1, 'item_standard', 1);
-
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('canteen_75', 'Canteen (75%)', 1, 1, 'item_standard', 1);
-
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('canteen_50', 'Canteen (50%)', 1, 1, 'item_standard', 1);
-
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('canteen_25', 'Canteen (25%)', 1, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('canteen', 'Canteen', 1, 1, 'item_standard', 1);
