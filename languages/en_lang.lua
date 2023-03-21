2e8f9be2447143f6a2b123a44497c051377b9348
JusCampin <37031311+JusCampin@users.noreply.github.com>
2023-03-21 02:12:24 -0700

    updated language

--- languages/en_lang.lua
+++ languages/en_lang.lua
@@ -1,12 +1,13 @@
 ---------------------------------- ENGLISH -------------------------------------------
 Locales["en_lang"] = {
 
-    filling = "Filling Canteen...",
+    filling = "Filling canteen...",
     full = "Your canteen is ~o~full",
-    canteen_75 = "Your canteen is ~o~3/4 ~q~full",
-    canteen_50 = "Your canteen is ~o~1/2 ~q~full",
-    canteen_25 = "Your canteen is ~o~1/4 ~q~full",
-    canteen_empty = "Your canteen is ~o~empty~q~!",
+    level4 = "Your canteen is ~o~75% ~q~full",
+    level3 = "Your canteen is ~o~50% ~q~full",
+    level2 = "Your canteen is ~o~25% ~q~full",
+    level1 = "Your canteen is ~o~empty~q~!",
     not_empty = "Your canteen is ~o~not ~q~empty",
     cantfill = "You can't fill your canteen here",
+    needcanteen = "You need a canteen!"
 }
