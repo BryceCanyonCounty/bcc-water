226fea4d0446750c4b6d0d32c02cc27d11200120
JusCampin <37031311+JusCampin@users.noreply.github.com>
2023-03-23 01:35:10 -0700

    update language

--- languages/en_lang.lua
+++ languages/en_lang.lua
@@ -2,6 +2,8 @@
 Locales["en_lang"] = {
 
     fill = "Fill",
+    wash = "Wash",
+    drink = "Drink",
     full = "Your canteen is ~o~full",
     level_4 = "Canteen is ~o~75 ~q~percent full",
     level_3 = "Canteen is ~o~50 ~q~percent full",
