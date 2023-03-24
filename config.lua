022b9a3b6c0a6bb2bf459cceecc4b1ff8b231494
JusCampin <37031311+JusCampin@users.noreply.github.com>
2023-03-24 01:59:25 -0700

    update config keys and locations

--- config.lua
+++ config.lua
@@ -3,13 +3,13 @@ Config = {}
 Config.defaultlang = "en_lang"
 
 -- Fill Canteen at Water Pumps, Rivers and Lakes
-Config.fillKey = 0x760A9C6F -- [G]
+Config.fillKey = 0xB2F377E8 -- [F]
 
 -- Wash Player in Rivers and Lakes
-Config.washKey = 0xC7B5340A -- [Enter]
+Config.washKey = 0xD9D0E1C0 -- [Space]
 
 -- Drink from Rivers and Lakes
-Config.drinkKey = 0xD9D0E1C0 -- [Space]
+Config.drinkKey = 0x760A9C6F -- [G]
 
 -- Show / Hide Canteen Level Messages
 Config.showMessages = true
@@ -17,32 +17,31 @@ Config.showMessages = true
 -- Amount to Increase Thirst in vorp_metabolism
 Config.thirst = 500 -- Default: 500
 
-Config.waterTypes = {
-    [1]  = { name = "Sea of Coronado",     waterhash = -247856387,  watertype = "lake"  },
-    [2]  = { name = "San Luis River",      waterhash = -1504425495, watertype = "river" },
-    [3]  = { name = "Lake Don Julio",      waterhash = -1369817450, watertype = "lake"  },
-    [4]  = { name = "Flat Iron Lake",      waterhash = -1356490953, watertype = "lake"  },
-    [5]  = { name = "Upper Montana River", waterhash = -1781130443, watertype = "river" },
-    [6]  = { name = "Owanjila",            waterhash = -1300497193, watertype = "river" },
-    [7]  = { name = "HawkEye Creek",       waterhash = -1276586360, watertype = "river" },
-    [8]  = { name = "Little Creek River",  waterhash = -1410384421, watertype = "river" },
-    [9]  = { name = "Dakota River",        waterhash =  370072007,  watertype = "river" },
-    [10] = { name = "Beartooth Beck",      waterhash =  650214731,  watertype = "river" },
-    [11] = { name = "Lake Isabella",       waterhash =  592454541,  watertype = "lake"  },
-    [12] = { name = "Cattail Pond",        waterhash = -804804953,  watertype = "lake"  },
-    [13] = { name = "Deadboot Creek",      waterhash =  1245451421, watertype = "river" },
-    [14] = { name = "Spider Gorge",        waterhash = -218679770,  watertype = "river" },
-    [15] = { name = "O'Creagh's Run",      waterhash = -1817904483, watertype = "lake"  },
-    [16] = { name = "Moonstone Pond",      waterhash = -811730579,  watertype = "lake"  },
-    [17] = { name = "Roanoke Valley",      waterhash = -1229593481, watertype = "river" },
-    [18] = { name = "Elysian Pool",        waterhash = -105598602,  watertype = "lake"  },
-    [19] = { name = "Heartland Overflow",  waterhash =  1755369577, watertype = "swamp" },
-    [20] = { name = "Lagras",              waterhash = -557290573,  watertype = "swamp" },
-    [21] = { name = "Lannahechee River",   waterhash = -2040708515, watertype = "river" },
-    [22] = { name = "Dakota River",        waterhash =  370072007,  watertype = "river" },
-    [23] = { name = "Random1",             waterhash =  231313522,  watertype = "river" },
-    [24] = { name = "Random2",             waterhash =  2005774838, watertype = "river" },
-    [25] = { name = "Random3",             waterhash = -1287619521, watertype = "river" },
-    [26] = { name = "Random4",             waterhash = -1308233316, watertype = "river" },
-    [27] = { name = "Random5",             waterhash = -196675805,  watertype = "river" },
+Config.locations = {
+    [1]  = { name = "Sea of Coronado",     hash = -247856387  },
+    [2]  = { name = "San Luis River",      hash = -1504425495 },
+    [3]  = { name = "Lake Don Julio",      hash = -1369817450 },
+    [4]  = { name = "Flat Iron Lake",      hash = -1356490953 },
+    [5]  = { name = "Upper Montana River", hash = -1781130443 },
+    [6]  = { name = "Owanjila",            hash = -1300497193 },
+    [7]  = { name = "HawkEye Creek",       hash = -1276586360 },
+    [8]  = { name = "Little Creek River",  hash = -1410384421 },
+    [9]  = { name = "Dakota River",        hash =  370072007  },
+    [10] = { name = "Beartooth Beck",      hash =  650214731  },
+    [11] = { name = "Lake Isabella",       hash =  592454541  },
+    [12] = { name = "Cattail Pond",        hash = -804804953  },
+    [13] = { name = "Deadboot Creek",      hash =  1245451421 },
+    [14] = { name = "Spider Gorge",        hash = -218679770  },
+    [15] = { name = "O'Creagh's Run",      hash = -1817904483 },
+    [16] = { name = "Moonstone Pond",      hash = -811730579  },
+    [17] = { name = "Roanoke Valley",      hash = -1229593481 },
+    [18] = { name = "Elysian Pool",        hash = -105598602  },
+    [19] = { name = "Heartland Overflow",  hash =  1755369577 },
+    [20] = { name = "Lagras",              hash = -557290573  },
+    [21] = { name = "Lannahechee River",   hash = -2040708515 },
+    [22] = { name = "Random1",             hash =  231313522  },
+    [23] = { name = "Random2",             hash =  2005774838 },
+    [24] = { name = "Random3",             hash = -1287619521 },
+    [25] = { name = "Random4",             hash = -1308233316 },
+    [26] = { name = "Random5",             hash = -196675805  }
 }
