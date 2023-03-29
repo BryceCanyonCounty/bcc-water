6998899ac983ca0c34f9f89454cd42d790d06b08
JusCampin <37031311+JusCampin@users.noreply.github.com>
2023-03-29 09:37:28 -0700

    German translation by MaffenTV_Steffen

---
+++ languages/de_lang.lua
@@ -0,0 +1,15 @@
+---------------------------------- German/Deutsch -------------------------------------------
+Locales["de_lang"] = {
+
+    fill = "Feldflasche auffüllen",
+    wash = "Waschen",
+    drink = "Trinken",
+    full = "Deine Feldflasche ist ~o~voll",
+    level_4 = "Feldflasche ist zu ~o~75 ~q~Prozent voll",
+    level_3 = "Feldflasche ist zu ~o~50 ~q~Prozent voll",
+    level_2 = "Feldflasche ist zu ~o~25 ~q~Prozent voll",
+    level_1 = "Deine Feldflasche ist ~o~leer~q~!",
+    not_empty = "Deine Feldflasche ist ~o~nicht ~q~leer",
+    cantfill = "Du kannst deine Feldflasche hier nicht auffüllen",
+    needcanteen = "Du brauchst eine Feldflasche!"
+}
