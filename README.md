054e826311712b32af634a7b1b000c9b9455acff
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-05-17 21:46:56 -0700

    add outsider_needs support (#17)

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Add the canteen to a store or crafting station for players to buy or make. Fill 
 #### Features
 - Carry a canteen of water to drink
 - Drink 4 times from a full canteen
-- Quenches thirst when using [vorp_metabolism](https://github.com/VORPCORE/vorp_metabolism-lua) or [fred_metabolism](https://github.com/SirFreddie/fred_metabolism_free)
+- Quenches thirst when using supported metabolism scripts
 - Refill canteen at waterpumps, rivers and lakes
 - Directly drink from rivers and lakes to save the water in your canteen
 - Separate health, stamina and thirst config values for drinking from canteen and wild waters
@@ -16,14 +16,20 @@ Add the canteen to a store or crafting station for players to buy or make. Fill 
 #### Dependencies
 - [vorp_core](https://github.com/VORPCORE/vorp-core-lua)
 - [vorp_inventory](https://github.com/VORPCORE/vorp_inventory-lua)
-- [vorp_metabolism](https://github.com/VORPCORE/vorp_metabolism-lua) or [fred_metabolism](https://github.com/SirFreddie/fred_metabolism_free)
+
+#### Supported Metablolism Scripts
+- VORP Metabolism
+- Fred Metabolism Free
+- Outsider Needs Metabolism
 
 #### Installation
 - Add `bcc-water` folder to your resources folder
 - Add `ensure bcc-water` to your `resources.cfg`
+- Ensure this script *after* your metabolism script
 - Run the included database file `water.sql`
 - Add canteen image to: `...\vorp_inventory\html\img`
 - Add canteen to a store or crafting station for player use
+- Set your metabolism script in the `config.lua` file
 
 #### Credits
 - green_canteen
--- server/server.lua
+++ server/server.lua
@@ -1,82 +1,102 @@
 local VORPcore = {}
 local VORPInv = {}
-TriggerEvent("getCore", function(core)
+local Needs = {}
+TriggerEvent('getCore', function(core)
     VORPcore = core
 end)
 
 VORPInv = exports.vorp_inventory:vorp_inventoryApi()
 
-RegisterNetEvent("bcc-water:CheckEmpty")
-AddEventHandler("bcc-water:CheckEmpty", function()
+TriggerEvent('Outsider_Needs', function(cb)
+    Needs = cb
+end)
+
+RegisterNetEvent('bcc-water:CheckEmpty')
+AddEventHandler('bcc-water:CheckEmpty', function()
 	local _source = source
-    local canteen = VORPInv.getItem(_source, "canteen")
+    local canteen = VORPInv.getItem(_source, 'canteen')
     if canteen ~= nil then
-        local meta = canteen["metadata"]
+        local meta = canteen['metadata']
         if next(meta) == nil then
-            VORPInv.subItem(_source, "canteen", 1, {})
-            VORPInv.addItem(_source, "canteen", 1, {description = "Level : Full", level = 5})
+            VORPInv.subItem(_source, 'canteen', 1, {})
+            VORPInv.addItem(_source, 'canteen', 1, {description = 'Level : Full', level = 5})
         else
             local level = meta.level
             if level == 1 then
-                VORPInv.subItem(_source, "canteen", 1, meta)
-                VORPInv.addItem(_source, "canteen", 1, {description = "Level : Full", level = 5})
+                VORPInv.subItem(_source, 'canteen', 1, meta)
+                VORPInv.addItem(_source, 'canteen', 1, {description = 'Level : Full', level = 5})
             else
-                VORPcore.NotifyRightTip(_source, _U("not_empty"), 5000)
+                VORPcore.NotifyRightTip(_source, _U('not_empty'), 5000)
                 TriggerClientEvent('bcc-water:Filling', _source)
                 return
             end
         end
-        TriggerClientEvent("bcc-water:FillCanteen", _source)
+        TriggerClientEvent('bcc-water:FillCanteen', _source)
     else
-        VORPcore.NotifyRightTip(_source, _U("needcanteen"), 5000)
+        VORPcore.NotifyRightTip(_source, _U('needcanteen'), 5000)
     end
 end)
 
-VORPInv.RegisterUsableItem("canteen", function(data)
+VORPInv.RegisterUsableItem('canteen', function(data)
     local _source = data.source
     VORPInv.CloseInv(_source)
     TriggerClientEvent('bcc-water:UseCanteen', _source, data)
 end)
 
-RegisterNetEvent("bcc-water:UpdateCanteen")
-AddEventHandler("bcc-water:UpdateCanteen", function(data)
+RegisterNetEvent('bcc-water:UpdateCanteen')
+AddEventHandler('bcc-water:UpdateCanteen', function(data)
     local _source = data.source
-    local canteen = VORPInv.getItem(_source, "canteen")
-    local meta = canteen["metadata"]
+    local canteen = VORPInv.getItem(_source, 'canteen')
+    local meta = canteen['metadata']
     local level = meta.level
     local message = nil
     local canteenUpdate = {
         [1] = function()
-            VORPcore.NotifyRightTip(_source, _U("level_1"), 5000)
+            VORPcore.NotifyRightTip(_source, _U('level_1'), 5000)
         end,
         [2] = function()
             message = 1
-            VORPInv.subItem(_source, "canteen", 1, meta)
-            VORPInv.addItem(_source, "canteen", 1, {description = "Level : Empty", level = 1})
+            VORPInv.subItem(_source, 'canteen', 1, meta)
+            VORPInv.addItem(_source, 'canteen', 1, {description = 'Level : Empty', level = 1})
         end,
         [3] = function()
             message = 2
-            VORPInv.subItem(_source, "canteen", 1, meta)
-            VORPInv.addItem(_source, "canteen", 1, {description = "Level : 25%", level = 2})
+            VORPInv.subItem(_source, 'canteen', 1, meta)
+            VORPInv.addItem(_source, 'canteen', 1, {description = 'Level : 25%', level = 2})
         end,
         [4] = function()
             message = 3
-            VORPInv.subItem(_source, "canteen", 1, meta)
-            VORPInv.addItem(_source, "canteen", 1, {description = "Level : 50%", level = 3})
+            VORPInv.subItem(_source, 'canteen', 1, meta)
+            VORPInv.addItem(_source, 'canteen', 1, {description = 'Level : 50%', level = 3})
         end,
         [5] = function()
             message = 4
-            VORPInv.subItem(_source, "canteen", 1, meta)
-            VORPInv.addItem(_source, "canteen", 1, {description = "Level : 75%", level = 4})
+            VORPInv.subItem(_source, 'canteen', 1, meta)
+            VORPInv.addItem(_source, 'canteen', 1, {description = 'Level : 75%', level = 4})
         end
     }
         if canteenUpdate[level] then
             canteenUpdate[level]()
         end
         if not level then
-            VORPcore.NotifyRightTip(_source, _U("level_1"), 5000)
+            VORPcore.NotifyRightTip(_source, _U('level_1'), 5000)
             return
         elseif level > 1 then
             TriggerClientEvent('bcc-water:Drink', _source, message)
         end
+end)
+
+RegisterNetEvent('outsider_needs:Thirst', function(wild)
+    local _source = source
+    local data = {}
+    if wild then
+        data = {
+            water = Config.otherWildThirst
+        }
+    else
+        data = {
+            water = Config.otherThirst
+        }
+    end
+    Needs.addStats(_source, data)
 end)
