080f772f5446462adee5b1f13de894aadd1647d3
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-06-17 01:25:59 -0700

    housekeeping

--- client/dataview.lua
+++ client/dataview.lua
@@ -1,32 +1,32 @@
 local _strblob = string.blob or function(length)
-    return string.rep("\0", math.max(40 + 1, length))
+    return string.rep('\0', math.max(40 + 1, length))
 end
 
 DataView = {
-    EndBig = ">",
-    EndLittle = "<",
+    EndBig = '>',
+    EndLittle = '<',
     Types = {
-        Int8 = { code = "i1", size = 1 },
-        Uint8 = { code = "I1", size = 1 },
-        Int16 = { code = "i2", size = 2 },
-        Uint16 = { code = "I2", size = 2 },
-        Int32 = { code = "i4", size = 4 },
-        Uint32 = { code = "I4", size = 4 },
-        Int64 = { code = "i8", size = 8 },
-        Uint64 = { code = "I8", size = 8 },
+        Int8 = { code = 'i1', size = 1 },
+        Uint8 = { code = 'I1', size = 1 },
+        Int16 = { code = 'i2', size = 2 },
+        Uint16 = { code = 'I2', size = 2 },
+        Int32 = { code = 'i4', size = 4 },
+        Uint32 = { code = 'I4', size = 4 },
+        Int64 = { code = 'i8', size = 8 },
+        Uint64 = { code = 'I8', size = 8 },
 
-        LuaInt = { code = "j", size = 8 },   -- a lua_Integer
-        UluaInt = { code = "J", size = 8 },  -- a lua_Unsigned
-        LuaNum = { code = "n", size = 8 },   -- a lua_Number
-        Float32 = { code = "f", size = 4 },  -- a float (native size)
-        Float64 = { code = "d", size = 8 },  -- a double (native size)
-        String = { code = "z", size = -1, }, -- zero terminated string
+        LuaInt = { code = 'j', size = 8 },   -- a lua_Integer
+        UluaInt = { code = 'J', size = 8 },  -- a lua_Unsigned
+        LuaNum = { code = 'n', size = 8 },   -- a lua_Number
+        Float32 = { code = 'f', size = 4 },  -- a float (native size)
+        Float64 = { code = 'd', size = 8 },  -- a double (native size)
+        String = { code = 'z', size = -1, }, -- zero terminated string
     },
 
     FixedTypes = {
-        String = { code = "c", size = -1, }, -- a fixed-sized string with n bytes
-        Int = { code = "i", size = -1, },    -- a signed int with n bytes
-        Uint = { code = "I", size = -1, },   -- an unsigned int with n bytes
+        String = { code = 'c', size = -1, }, -- a fixed-sized string with n bytes
+        Int = { code = 'i', size = -1, },    -- a signed int with n bytes
+        Uint = { code = 'I', size = -1, },   -- an unsigned int with n bytes
     },
 }
 DataView.__index = DataView
@@ -66,7 +66,7 @@ end
 
 --[[ Create the API by using DataView.Types. --]]
 for label, datatype in pairs(DataView.Types) do
-    DataView["Get" .. label] = function(self, offset, endian)
+    DataView['Get' .. label] = function(self, offset, endian)
         local o = self.offset + offset
         if _ib(o, self.length, datatype) then
             local v, _ = string.unpack(_ef(endian) .. datatype.code, self.blob, o)
@@ -75,7 +75,7 @@ for label, datatype in pairs(DataView.Types) do
         return nil -- Out of bounds
     end
 
-    DataView["Set" .. label] = function(self, offset, value, endian)
+    DataView['Set' .. label] = function(self, offset, value, endian)
         local o = self.offset + offset
         if _ib(o, self.length, datatype) then
             return SetFixed(self, o, value, _ef(endian) .. datatype.code)
@@ -85,27 +85,27 @@ for label, datatype in pairs(DataView.Types) do
 
     -- Ensure cache is correct.
     if datatype.size >= 0 and string.packsize(datatype.code) ~= datatype.size then
-        local msg = "Pack size of %s (%d) does not match cached length: (%d)"
+        local msg = 'Pack size of %s (%d) does not match cached length: (%d)'
         error(msg:format(label, string.packsize(fmt[#fmt]), datatype.size))
         return nil
     end
 end
 
 for label, datatype in pairs(DataView.FixedTypes) do
-    DataView["GetFixed" .. label] = function(self, offset, typelen, endian)
+    DataView['GetFixed' .. label] = function(self, offset, typelen, endian)
         local o = self.offset + offset
         if o + (typelen - 1) <= self.length then
-            local code = _ef(endian) .. "c" .. tostring(typelen)
+            local code = _ef(endian) .. 'c' .. tostring(typelen)
             local v, _ = string.unpack(code, self.blob, o)
             return v
         end
         return nil -- Out of bounds
     end
 
-    DataView["SetFixed" .. label] = function(self, offset, typelen, value, endian)
+    DataView['SetFixed' .. label] = function(self, offset, typelen, value, endian)
         local o = self.offset + offset
         if o + (typelen - 1) <= self.length then
-            local code = _ef(endian) .. "c" .. tostring(typelen)
+            local code = _ef(endian) .. 'c' .. tostring(typelen)
             return SetFixed(self, o, value, code)
         end
         return self
@@ -120,7 +120,7 @@ SetFixed = function(self, offset, value, code)
     -- All bytes prior to the offset
     if self.offset < offset then
         local size = offset - self.offset
-        fmt[#fmt + 1] = "c" .. tostring(size)
+        fmt[#fmt + 1] = 'c' .. tostring(size)
         values[#values + 1] = self.blob:sub(self.offset, size)
     end
 
@@ -134,11 +134,11 @@ SetFixed = function(self, offset, value, code)
         local newoff = offset + ps
         local size = self.length - newoff + 1
 
-        fmt[#fmt + 1] = "c" .. tostring(size)
+        fmt[#fmt + 1] = 'c' .. tostring(size)
         values[#values + 1] = self.blob:sub(newoff, self.length)
     end
 
-    self.blob = string.pack(table.concat(fmt, ""), table.unpack(values))
+    self.blob = string.pack(table.concat(fmt, ''), table.unpack(values))
     self.length = self.blob:len()
     return self
 end
