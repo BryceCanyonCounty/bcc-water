1adb76827bf83ccd00043b79d11daf8964278462
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-10-15 21:47:49 -0700

    Multi updates (#33)
    * Update config.lua
    
    - add support for Fred Metabolism v2
    - add pump and wild water configs
    
    * Update server.lua
    
    - change to new inventory api
    - change to new callback api
    - housekeeping
    
    * Update client.lua
    
    - change to new callback api
    - update prompts
    - add support for Fred Meta v2
    - add multiple config options
    - changed main thread structure
    - housekeeping

--- server/server.lua
+++ server/server.lua
@@ -1,31 +1,31 @@
 local VORPcore = {}
-local VORPInv = {}
-local Needs = {}
-
 TriggerEvent('getCore', function(core)
     VORPcore = core
 end)
+
+local Needs = {}
 TriggerEvent('Outsider_Needs', function(cb)
     Needs = cb
 end)
-VORPInv = exports.vorp_inventory:vorp_inventoryApi()
 
-VORPcore.addRpcCallback('GetCanteenLevel', function(source, cb)
+local ServerRPC = exports.vorp_core:ServerRpcCall()
+
+ServerRPC.Callback.Register('bcc-water:GetCanteenLevel', function(source, cb)
 	local _source = source
     local itemCanteen = Config.canteen
-    local canteen = VORPInv.getItem(_source, itemCanteen)
+    local canteen = exports.vorp_inventory:getItem(_source, itemCanteen)
     if canteen ~= nil then
         local meta = canteen['metadata']
         if next(meta) == nil then
-            VORPInv.subItem(_source, itemCanteen, 1, {})
-            VORPInv.addItem(_source, itemCanteen, 1, {
+            exports.vorp_inventory:subItem(_source, itemCanteen, 1, {})
+            exports.vorp_inventory:addItem(_source, itemCanteen, 1, {
                 description = Config.lang.level .. ' : ' .. '<span style=color:green;>' .. Config.lang.full .. '</span>', level = 5
             })
         else
             local level = meta.level
             if level < 5 then
-                VORPInv.subItem(_source, itemCanteen, 1, meta)
-                VORPInv.addItem(_source, itemCanteen, 1, {
+                exports.vorp_inventory:subItem(_source, itemCanteen, 1, meta)
+                exports.vorp_inventory:addItem(_source, itemCanteen, 1, {
                     description = Config.lang.level .. ' : ' .. '<span style=color:green;>' .. Config.lang.full .. '</span>', level = 5
                 })
             else
@@ -42,16 +42,16 @@ VORPcore.addRpcCallback('GetCanteenLevel', function(source, cb)
     end
 end)
 
-VORPInv.RegisterUsableItem(Config.canteen, function(data)
+exports.vorp_inventory:registerUsableItem(Config.canteen, function(data)
     local _source = data.source
-    VORPInv.CloseInv(_source)
+    exports.vorp_inventory:closeInventory(_source)
     TriggerClientEvent('bcc-water:UseCanteen', _source)
 end)
 
-VORPcore.addRpcCallback('UpdateCanteen', function(source, cb)
+ServerRPC.Callback.Register('bcc-water:UpdateCanteen', function(source, cb)
     local _source = source
     local itemCanteen = Config.canteen
-    local canteen = VORPInv.getItem(_source, itemCanteen)
+    local canteen = exports.vorp_inventory:getItem(_source, itemCanteen)
     local meta = canteen['metadata']
     local level = meta.level
     local canteenUpdate = {
@@ -59,48 +59,48 @@ VORPcore.addRpcCallback('UpdateCanteen', function(source, cb)
             VORPcore.NotifyRightTip(_source, _U('message_1'), 4000)
         end,
         [2] = function()
-            VORPInv.subItem(_source, itemCanteen, 1, meta)
-            VORPInv.addItem(_source, itemCanteen, 1, {
+            exports.vorp_inventory:subItem(_source, itemCanteen, 1, meta)
+            exports.vorp_inventory:addItem(_source, itemCanteen, 1, {
                 description = Config.lang.level .. ' : ' .. '<span style=color:red;>' .. Config.lang.empty .. '</span>', level = 1
             })
         end,
         [3] = function()
-            VORPInv.subItem(_source, itemCanteen, 1, meta)
-            VORPInv.addItem(_source, itemCanteen, 1, {
+            exports.vorp_inventory:subItem(_source, itemCanteen, 1, meta)
+            exports.vorp_inventory:addItem(_source, itemCanteen, 1, {
                 description = Config.lang.level .. ' : ' .. '<span style=color:green;>' .. '25%' .. '</span>', level = 2
             })
         end,
         [4] = function()
-            VORPInv.subItem(_source, itemCanteen, 1, meta)
-            VORPInv.addItem(_source, itemCanteen, 1, {
+            exports.vorp_inventory:subItem(_source, itemCanteen, 1, meta)
+            exports.vorp_inventory:addItem(_source, itemCanteen, 1, {
                 description = Config.lang.level .. ' : ' .. '<span style=color:green;>' .. '50%' .. '</span>', level = 3
             })
         end,
         [5] = function()
-            VORPInv.subItem(_source, itemCanteen, 1, meta)
-            VORPInv.addItem(_source, itemCanteen, 1, {
+            exports.vorp_inventory:subItem(_source, itemCanteen, 1, meta)
+            exports.vorp_inventory:addItem(_source, itemCanteen, 1, {
                 description = Config.lang.level .. ' : ' .. '<span style=color:green;>' .. '75%' .. '</span>', level = 4
             })
         end
     }
     if canteenUpdate[level] then
-	canteenUpdate[level]()
-        end
-        if not level then
-            VORPcore.NotifyRightTip(_source, _U('message_1'), 4000)
-            cb(false)
-        elseif level > 1 then
-            cb(level)
-        end
+	    canteenUpdate[level]()
+    end
+    if not level then
+        VORPcore.NotifyRightTip(_source, _U('message_1'), 4000)
+        cb(false)
+    elseif level > 1 then
+        cb(level)
+    end
 end)
 
 -- Check if Player has an Empty Bucket and Update
-VORPcore.addRpcCallback('GetBucketLevel', function(source, cb)
+ServerRPC.Callback.Register('bcc-water:GetBucket', function(source, cb)
     local _source = source
-    local itemCount = VORPInv.getItemCount(_source, Config.emptyBucket)
-    if itemCount > 0 then
-        VORPInv.subItem(_source, Config.emptyBucket, 1)
-        VORPInv.addItem(_source, Config.fullBucket, 1)
+    local hasItem = exports.vorp_inventory:getItem(_source, Config.emptyBucket)
+    if hasItem then
+        exports.vorp_inventory:subItem(_source, Config.emptyBucket, 1)
+        exports.vorp_inventory:addItem(_source, Config.fullBucket, 1)
         cb(true)
     else
         VORPcore.NotifyRightTip(_source, _U('needBucket'))
