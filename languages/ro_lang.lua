a507d93a49d39d6f786fe27d01fb81c3bea0f799
iseeyoucopy <iseeyoucopy@yahoo.com>
2024-04-06 20:51:42 +0100

    update ro_lang.lua
    Adjust the diacritics in the language file  that are not displaying correctly.

--- languages/ro_lang.lua
+++ languages/ro_lang.lua
@@ -2,17 +2,17 @@
 Locales["ro_lang"] = {
 
     fillCanteen = "Umple Pleosca",
-    fillBucket = "Umple Găleata",
-    wash = "Spală-te",
+    fillBucket = "Umple Galeata",
+    wash = "Spala-te",
     drink = "Bea",
     fullCanteen = "Pleosca ta este plina.",
     fullBucket = "Galeata ta este plina.",
-    message_4 = "Pleosca este ~o~75 ~q~la sută plină.",
-    message_3 = "Pleosca este ~o~50 ~q~la sută plină.",
-    message_2 = "Pleosca este ~o~25 ~q~la sută plină.",
-    message_1 = "Pleosca ta este goală!",
-    notEmpty = "Pleosca ta nu este goală.",
+    message_4 = "Pleosca este ~o~75 ~q~la suta plina.",
+    message_3 = "Pleosca este ~o~50 ~q~la suta plina.",
+    message_2 = "Pleosca este ~o~25 ~q~la suta plina.",
+    message_1 = "Pleosca ta este goala!",
+    notEmpty = "Pleosca ta nu este goala.",
     needCanteen = "Ai nevoie de o pleosca!",
     needBucket = "Ai nevoie de o galeata goala!",
-    waterPump = "Pompă de Apă"
+    waterPump = "Pompa de Apa"
 }
