680f201e8a8d2189db2f83c5296023dd32e8ed90
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-03-19 14:42:56 -0700

    Update ptbr_lang.lua

--- languages/ptbr_lang.lua
+++ languages/ptbr_lang.lua
@@ -1,18 +1,19 @@
----------------- PORTUGUÊS BRASILEIRO ----------------
 Locales["ptbr_lang"] = {
 
     fillCanteen = "Encher Cantil",
     fillBucket = "Encher Balde",
+    fillBottle = "Encher Garrafa",
     wash = "Lavar",
     drink = "Beber",
     fullCanteen = "Seu cantil está cheio.",
     fullBucket = "Seu balde está cheio.",
-    message_4 = "Cantil está ~o~75 ~q~por cento cheio.",
-    message_3 = "Cantil está ~o~50 ~q~por cento cheio.",
-    message_2 = "Cantil está ~o~25 ~q~por cento cheio.",
-    message_1 = "Seu cantil está vazio!",
-    notEmpty = "Seu cantil não está vazio.",
+    fullBottle = "Sua garrafa está cheia.",
+    emptyCanteen = "Seu cantil está vazio!",
     needCanteen = "Você precisa de um cantil!",
     needBucket = "Você precisa de um balde vazio!",
-    waterPump = "Bomba de Água"
+    needBottle = "Você precisa de uma garrafa vazia!",
+    waterPump = "Bomba de Água",
+    Durability = "Durability",
+    brokeCanteen = "Your canteen has broken!",
+    drinksLeft = "Drinks Left",
 }
