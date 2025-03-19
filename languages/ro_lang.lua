07eec4b4195208736fe1df5a115fb7e1a32f018b
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-03-19 14:42:59 -0700

    Update ro_lang.lua

--- languages/ro_lang.lua
+++ languages/ro_lang.lua
@@ -1,18 +1,19 @@
----------------------------------- ROMANIAN -------------------------------------------
 Locales["ro_lang"] = {
 
     fillCanteen = "Umple Pleosca",
     fillBucket = "Umple Galeata",
+    fillBottle = "Umple Sticla",
     wash = "Spala-te",
     drink = "Bea",
     fullCanteen = "Pleosca ta este plina.",
     fullBucket = "Galeata ta este plina.",
-    message_4 = "Pleosca este ~o~75 ~q~la suta plina.",
-    message_3 = "Pleosca este ~o~50 ~q~la suta plina.",
-    message_2 = "Pleosca este ~o~25 ~q~la suta plina.",
-    message_1 = "Pleosca ta este goala!",
-    notEmpty = "Pleosca ta nu este goala.",
+    fullBottle = "Sticla ta este plina.",
+    emptyCanteen = "Pleosca ta este goala!",
     needCanteen = "Ai nevoie de o pleosca!",
     needBucket = "Ai nevoie de o galeata goala!",
-    waterPump = "Pompa de Apa"
+    needBottle = "Ai nevoie de o sticla goala!",
+    waterPump = "Pompa de Apa",
+    Durability = "Durability",
+    brokeCanteen = "Your canteen has broken!",
+    drinksLeft = "Drinks Left",
 }
