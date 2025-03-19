c8ac986b960660f97190ce835ec422320aeec537
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-03-19 14:44:17 -0700

    Update water.sql
    - updated format
    - added bottle items
    - added descriptions to items

--- water.sql
+++ water.sql
@@ -1,8 +1,15 @@
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('canteen', 'Canteen', 1, 1, 'item_standard', 1)
-    ON DUPLICATE KEY UPDATE `item`='canteen', `label`='Canteen', `limit`=1, `can_remove`=1, `type`='item_standard', `usable`=1;
-
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('wateringcan', 'Water Jug', 10, 1, 'item_standard', 1)
-    ON DUPLICATE KEY UPDATE `item`='wateringcan', `label`='Water Jug', `limit`=10, `can_remove`=1, `type`='item_standard', `usable`=1;
-
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('wateringcan_empty', 'Empty Watering Jug', 10, 1, 'item_standard', 1)
-    ON DUPLICATE KEY UPDATE `item`='wateringcan_empty', `label`='Empty Watering Jug', `limit`=10, `can_remove`=1, `type`='item_standard', `usable`=1;
+INSERT INTO `items` (`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `desc`)
+VALUES
+    ('canteen', 'Canteen', 1, 1, 'item_standard', 1, 'Portable container to carry water'),
+    ('wateringcan', 'Water Jug', 10, 1, 'item_standard', 1, 'Bucket of water'),
+    ('wateringcan_empty', 'Empty Watering Jug', 10, 1, 'item_standard', 1, 'Empty water bucket'),
+    ('bcc_empty_bottle', 'Empty Bottle', 15, 1, 'item_standard', 1, 'Empty bottle'),
+    ('bcc_full_bottle', 'Water Bottle', 15, 1, 'item_standard', 1, 'Bottle of water')
+ON DUPLICATE KEY UPDATE
+    `item` = VALUES(`item`),
+    `label` = VALUES(`label`),
+    `limit` = VALUES(`limit`),
+    `can_remove` = VALUES(`can_remove`),
+    `type` = VALUES(`type`),
+    `usable` = VALUES(`usable`),
+    `desc` = VALUES(`desc`);
