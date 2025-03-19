8a20b2b4ae8dd961c8a36a82a00a7dd538318e73
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-03-19 14:42:45 -0700

    Update en_lang.lua

--- languages/en_lang.lua
+++ languages/en_lang.lua
@@ -1,18 +1,19 @@
----------------------------------- ENGLISH -------------------------------------------
 Locales["en_lang"] = {
 
     fillCanteen = "Fill Canteen",
     fillBucket = "Fill Bucket",
+    fillBottle = "Fill Bottle",
     wash = "Wash",
     drink = "Drink",
     fullCanteen = "Your canteen is full.",
     fullBucket = "Your bucket is full.",
-    message_4 = "Canteen is ~o~75 ~q~percent full.",
-    message_3 = "Canteen is ~o~50 ~q~percent full.",
-    message_2 = "Canteen is ~o~25 ~q~percent full.",
-    message_1 = "Your canteen is empty!",
-    notEmpty = "Your canteen is not empty.",
+    fullBottle = "Your bottle is full.",
+    emptyCanteen = "Your canteen is empty!",
     needCanteen = "You need a canteen!",
     needBucket = "You need an empty bucket!",
-    waterPump = "Water Pump"
+    needBottle = "You need an empty bottle!",
+    waterPump = "Water Pump",
+    Durability = "Durability",
+    brokeCanteen = "Your canteen has broken!",
+    drinksLeft = "Drinks Left",
 }
