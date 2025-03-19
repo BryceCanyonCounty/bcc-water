ee7b27c513958ebc8192c9b72aa91eabe7137bab
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-03-19 14:47:52 -0700

    Update main.lua
    - added devMode settings
    - key table now handles chars for using DrawText option
    - many settings moved into tables for better organization
    - added setting for number of drinks from full canteen

--- config/main.lua
+++ config/main.lua
@@ -1,101 +1,111 @@
-Config = {}
+Config = {
 
-Config.defaultlang = 'en_lang'
----------------------------------------------------------------
+    defaultlang      = 'en_lang',
+    ---------------------------------------------------------------
 
--- Vorp Metabolism              = 1
--- Fred Metabolism Free         = 2
--- Outsider Needs               = 3
--- Fred Metabolism Paid v1.4    = 4
--- Fred Metabolism Paid v2      = 5
--- RSD Metabolism Paid          = 6
--- NXT Metabolism Paid          = 7
--- Andrade Metabolism Paid      = 8
--- FX-HUD                       = 9
-Config.app = 1
----------------------------------------------------------------
+    devMode = {
+        active  = false,      -- When active, you can restart the script while connected for testing (otherwise requires relog)
+        command = 'WaterDev', -- Command to start the script functions
+    },
+    ---------------------------------------------------------------
 
-Config.keys = {
-    drink       = 0x760A9C6F, -- [G] Drink at Water Pumps and Wild Waters
-    wash        = 0x80F28E95, -- [L] Wash Player at Water Pumps and Wild Waters
-    fillBucket  = 0xCEFD9220, -- [E] Fill Bucket at Water Pumps and Wild Waters
-    fillCanteen = 0x27D1C284, -- [R] Fill Canteen at Water Pumps and Wild Waters
+    -- Vorp Metabolism              = 1
+    -- Fred Metabolism Free         = 2
+    -- Outsider Needs               = 3
+    -- Fred Metabolism Paid v1.4    = 4
+    -- Fred Metabolism Paid v2      = 5
+    -- RSD Metabolism Paid          = 6
+    -- NXT Metabolism Paid          = 7
+    -- Andrade Metabolism Paid      = 8
+    -- FX-HUD                       = 9
+    app = 1,
+    ---------------------------------------------------------------
+
+    keys = {
+        drink       = { code = 0x760A9C6F, char = 'G' }, -- Drink at Water Pumps and Wild Waters
+        wash        = { code = 0x80F28E95, char = 'L' }, -- Wash Player at Water Pumps and Wild Waters
+        fillBucket  = { code = 0xCEFD9220, char = 'E' }, -- Fill Bucket at Water Pumps and Wild Waters
+        fillBottle  = { code = 0xD3ECF82F, char = 'B' }, -- Fill Bottle at Water Pumps and Wild Waters
+        fillCanteen = { code = 0x27D1C284, char = 'R' }  -- Fill Canteen at Water Pumps and Wild Waters
+    },
+    ---------------------------------------------------------------
+
+    -- Manage Activities at Water Pumps and Other Objects
+    pump = {
+        active  = true, -- Enable the Use of Water Pumps and Other Objects
+        canteen = true, -- Allow to Fill Canteen
+        bucket  = true, -- Allow to Fill Bucket
+        bottle  = true, -- Allow to Fill Bottle
+        wash    = true, -- Allow Player to Wash
+        drink   = true, -- Allow Player to Drink
+    },
+    ---------------------------------------------------------------
+
+    -- Manage Activities in Rivers and Lakes (Wild Waters)
+    wild = {
+        active  = true, -- Enable the Use of Rivers and Lakes (Wild Waters)
+        canteen = true, -- Allow to Fill Canteen
+        bucket  = true, -- Allow to Fill Bucket
+        bottle  = true, -- Allow to Fill Bottle
+        wash    = true, -- Allow Player to Wash
+        drink   = true, -- Allow Player to Drink
+    },
+    ---------------------------------------------------------------
+
+    -- Item Names from Database
+    canteen     = 'canteen',
+
+    emptyBucket = 'wateringcan_empty',
+    fullBucket  = 'wateringcan',
+
+    emptyBottle = 'bcc_empty_bottle',
+    fullBottle  = 'bcc_full_bottle',
+    ---------------------------------------------------------------
+
+    durability = {
+        canteen = 20, -- Durability used per Drink and Fill
+    },
+    ---------------------------------------------------------------
+
+    -- Objects Where You Can Get Water
+    objects = {
+        'p_waterpump01x',
+        'p_wellpumpnbx01x',
+        'p_sink02x',
+        'p_drysink01x',
+        'p_sink03x',
+        'p_barrel_ladle01x',
+        'p_barrel_wash01x',
+    },
+    ---------------------------------------------------------------
+
+    -- Boosts Drinking from Canteen
+    canteenDrink = {
+        health  = 10, -- Default: 10 / Value is 0 - 100 / Set to 0 to Disable
+        stamina = 20, -- Default: 20 / Value is 0 - 100 / Set to 0 to Disable
+        thirst  = 50  -- Default: 50 / Value is 0 - 100
+    },
+
+    -- Effects Drinking from Wild Waters
+    wildDrink = {
+        gainHealth  = true, -- true = Gain Health by health Value / false = Lose by Value
+        health      = 5,    -- Default: 5 / Value is 0 - 100 / Set to 0 to Disable
+        gainStamina = true, -- true = Gain Stamina by stamina Value / false = Lose by Value
+        stamina     = 10,   -- Default: 10 / Value is 0 - 100 / Set to 0 to Disable
+        thirst      = 25    -- Default: 25 / Value is 0 - 100
+    },
+    ---------------------------------------------------------------
+
+    maxCanteenDrinks = 5, -- Maximum Drinks per Full Canteen
+    ---------------------------------------------------------------
+
+    -- Crouch in Wild Water to Show Prompts
+    crouch = true, -- Default: true / Set to false to Remove Crouch Requirement
+    ---------------------------------------------------------------
+
+    usePrompt = true, -- true = Show Prompt Button at Water Pumps / false = Show Text at Water Pumps (no button)
+    ---------------------------------------------------------------
+
+    showMessages = true, -- Show / Hide Canteen Messages
+    ---------------------------------------------------------------
 }
----------------------------------------------------------------
-
-Config.pumpsEnabled = true -- Enable the Use of Water Pumps
-
-Config.pumpCanteen  = true -- Fill Canteen at Pumps
-Config.pumpBucket   = true -- Fill Bucket at Pumps
-Config.pumpWash     = true -- Wash at Pumps
-Config.pumpDrink    = true -- Drink at Pumps
----------------------------------------------------------------
-
-Config.wildEnabled = true -- Enable the Use of Rivers and Lakes (Wild Waters)
-
-Config.wildCanteen = true -- Fill Canteen in Wild Waters
-Config.wildBucket  = true -- Fill Bucket in Wild Waters
-Config.wildWash    = true -- Wash in Wild Waters
-Config.wildDrink   = true -- Drink Directly from Wild Waters
----------------------------------------------------------------
-
--- Item Names from Database
-Config.emptyBucket = 'wateringcan_empty'
-
-Config.fullBucket  = 'wateringcan'
-
-Config.canteen     = 'canteen'
----------------------------------------------------------------
-
--- Crouch in Wild Water to Show Prompts
-Config.crouch = true -- Default: true / Set to false to Remove Crouch Requirement
----------------------------------------------------------------
-
-Config.usePrompt = true -- true = Show Prompt Button at Water Pumps / false = Show Text at Water Pumps (no button)
----------------------------------------------------------------
-
-Config.showMessages = true -- Show / Hide Canteen Level Messages
----------------------------------------------------------------
-
--- Boosts Drinking from Canteen
-Config.health  = 10 -- Default: 10 / Value is 0 - 100 / Set to 0 to Disable
-
-Config.stamina = 20 -- Default: 20 / Value is 0 - 100 / Set to 0 to Disable
-
-Config.thirst  = 50 -- Default: 50 / Value is 0 - 100
----------------------------------------------------------------
-
--- Effects Drinking from Wild Waters
-Config.gainHealth = true -- true = Gain Health by wildHealth Value / false = Lose by Value
-Config.wildHealth  = 5  -- Default: 5 / Value is 0 - 100 / Set to 0 to Disable
-
-Config.gainStamina = true -- true = Gain Stamina by wildStamina Value / false = Lose by Value
-Config.wildStamina = 10 -- Default: 10 / Value is 0 - 100 / Set to 0 to Disable
-
-Config.wildThirst  = 25 -- Default: 25 / Value is 0 - 100
----------------------------------------------------------------
-
-Config.CanteenUsage = 5 -- Default: 5 / Durability used per Drink
----------------------------------------------------------------
-
--- Translate Canteen Metadata
-Config.lang = {
-    level = 'Level',
-    empty = 'Empty',
-    full  = 'Full',
-    Durability = 'Durability',
-}
----------------------------------------------------------------
-
--- Objects Where You Can Get Water
-Config.objects = {
-    'p_waterpump01x',
-    'p_wellpumpnbx01x',
-    'p_sink02x',
-    'p_drysink01x',
-    'p_drysink01x',
-    'p_sink03x',
-    'p_barrel_ladle01x',
-    'p_barrel_wash01x',
-}
----------------------------------------------------------------
