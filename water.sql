397dad6dbba62af2185c845818a20785e7c1e2b2
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-03-20 22:27:04 -0700

    Update water.sql

--- water.sql
+++ water.sql
@@ -1,10 +1,10 @@
 INSERT INTO `items` (`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `desc`)
 VALUES
-    ('canteen', 'Canteen', 1, 1, 'item_standard', 1, 'Portable container to carry water'),
-    ('wateringcan', 'Water Jug', 10, 1, 'item_standard', 1, 'Bucket of water'),
-    ('wateringcan_empty', 'Empty Watering Jug', 10, 1, 'item_standard', 1, 'Empty water bucket'),
-    ('bcc_empty_bottle', 'Empty Bottle', 15, 1, 'item_standard', 1, 'Empty bottle'),
-    ('bcc_full_bottle', 'Water Bottle', 15, 1, 'item_standard', 1, 'Bottle of water')
+    ('canteen', 'Canteen', 1, 1, 'item_standard', 1, 'A portable container to carry water.'),
+    ('wateringcan', 'Water Jug', 10, 1, 'item_standard', 1, 'A bucket of water.'),
+    ('wateringcan_empty', 'Empty Watering Jug', 10, 1, 'item_standard', 1, 'An empty water bucket.'),
+    ('bcc_empty_bottle', 'Empty Bottle', 15, 1, 'item_standard', 1, 'An empty bottle.'),
+    ('bcc_full_bottle', 'Water Bottle', 15, 1, 'item_standard', 1, 'A bottle of water.')
 ON DUPLICATE KEY UPDATE
     `item` = VALUES(`item`),
     `label` = VALUES(`label`),
